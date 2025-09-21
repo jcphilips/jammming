547651f086eba4f545d95f88d6fb6fa8262f89c6
Jarrod Philips <4137235+jcphilips@users.noreply.github.com>
2025-09-21 17:58:52 +0530

    fix: prop names mismatch not updating state

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Search(props) {
 
-  const { search, handleSearch, handleSubmit } = props;
+  const { input, handleInput, handleSubmit } = props;
 
   return (
    <form onSubmit={ handleSubmit }>
-      <input type="text" id="search" name="search" value={search} onChange={handleSearch} />
+      <input type="text" id="search" name="search" value={input} onChange={handleInput} />
       <input type="submit" name="submit" value="Search" />
    </form>
   )
